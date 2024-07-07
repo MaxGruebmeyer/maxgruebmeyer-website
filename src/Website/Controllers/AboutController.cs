05b10b09f6074a370c298b1d8f1014a6aa46d609
Max Gruebmeyer <max@gruebmeyer.de>
2024-07-07 13:50:01 +0200

    Some more progress on website content

--- src/Website/Controllers/AboutController.cs
+++ src/Website/Controllers/AboutController.cs
@@ -11,12 +11,6 @@ public class AboutController : Controller
         _logger = logger;
     }
 
-    // TODO (GM):
-    // - Projects Page
-    // - Link to GitHub
-    // - CV
-    // - Contact Me (Contact Information + Mail Sending Service)
-
     public IActionResult Index()
     {
         return View();
