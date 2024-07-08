56eab9bd29e458ac0a184b12642fd9bf1e4bf879
Max Gruebmeyer <max@gruebmeyer.de>
2024-07-08 13:04:29 +0200

    Started to implement light mode

--- src/Website/tailwind.config.js
+++ src/Website/tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-       './Pages/**/*.cshtml',
-       './Views/**/*.cshtml'
-],
-    theme: {
-        extend: {
-            aspectRatio: {
-                '4/3': '4 / 3',
-                '16/9': '16 / 9',
+    module.exports = {
+        darkMode: 'selector',
+        content: [
+            './Pages/**/*.cshtml',
+            './Views/**/*.cshtml'
+        ],
+        theme: {
+            extend: {
+                aspectRatio: {
+                    '4/3': '4 / 3',
+                    '16/9': '16 / 9',
+                }
             }
-        }
-    },
-    plugins: [
-    ],
-}
+        },
+        plugins: [
+        ],
+    }
