ce9b9493394d35d0d177d7b1256ee54b4f73ab6f
Max Gruebmeyer <max@gruebmeyer.de>
2024-07-08 15:53:49 +0200

    Created small grid for recommended videos

--- src/Website/tailwind.config.js
+++ src/Website/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
     module.exports = {
-        darkMode: 'class',
+        darkMode: 'selector',
         content: [
             './Pages/**/*.cshtml',
             './Views/**/*.cshtml'
